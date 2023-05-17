8591b2ce93ad7659098723b58d530879304e3f20
Hemalatha Elangovan <helangovan@pandora.com>
2023-05-16 23:11:51 -0700

    add s3

--- aws-app-cdk/lib/aws-app-cdk-stack.ts
+++ aws-app-cdk/lib/aws-app-cdk-stack.ts
@@ -1,4 +1,4 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import {aws_s3, Duration, Stack, StackProps} from 'aws-cdk-lib';
 import * as sns from 'aws-cdk-lib/aws-sns';
 import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -15,5 +15,11 @@ export class AwsAppCdkStack extends Stack {
     // const topic = new sns.Topic(this, 'AwsAppCdkTopic');
     //
     // topic.addSubscription(new subs.SqsSubscription(queue));
+
+   const bucket = new aws_s3.Bucket(this,"test-cdk",{
+     bucketName: "hema2023051623",
+     versioned: false,
+     blockPublicAccess: aws_s3.BlockPublicAccess.BLOCK_ALL
+   });
   }
 }
