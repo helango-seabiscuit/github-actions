d10c15ffc0e02d6c44a4185956149d7bf6344e55
Hemalatha Elangovan <helangovan@pandora.com>
2023-05-31 23:00:22 -0700

    add java app

---
+++ reactive-music/src/test/java/com/example/reactivemusic/ReactiveMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactivemusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
