059f46584061d3be22301f2d7b43cf55a614fce4
Hemalatha Elangovan <helangovan@pandora.com>
2023-06-05 23:03:51 -0700

    add reactive rest test controller

--- aws-app-cdk/lib/aws-app-cdk-stack.ts
+++ aws-app-cdk/lib/aws-app-cdk-stack.ts
@@ -1,7 +1,10 @@
-import {aws_s3, Duration, Stack, StackProps} from 'aws-cdk-lib';
+import {aws_ecr, aws_s3, Duration, Stack, StackProps} from 'aws-cdk-lib';
 import * as sns from 'aws-cdk-lib/aws-sns';
 import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecs_patterns from 'aws-cdk-lib/aws-ecs-patterns';
 import { Construct } from 'constructs';
 
 export class AwsAppCdkStack extends Stack {
@@ -21,5 +24,22 @@ export class AwsAppCdkStack extends Stack {
      versioned: false,
      blockPublicAccess: aws_s3.BlockPublicAccess.BLOCK_ALL
    });
+
+   const vpc = new ec2.Vpc(this, "MyVpc",{
+       maxAzs: 3
+   });
+
+   const cluster = new ecs.Cluster(this, "MyCluster",{
+       vpc: vpc
+   });
+
+   new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService",{
+       cluster: cluster,
+       cpu: 5256,
+       desiredCount: 2,
+       memoryLimitMiB: 512,
+       publicLoadBalancer: true,
+       taskImageOptions: { image: ecs.ContainerImage.fromEcrRepository(aws_ecr.Repository.fromRepositoryName(this,"repository","reactive-music"))}
+   })
   }
 }
