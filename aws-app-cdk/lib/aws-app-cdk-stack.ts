01205cec87df42fa60e1035f9ba61d04c5b08459
Hemalatha Elangovan <helangovan@pandora.com>
2023-06-17 18:34:19 -0700

    s3 bucket test 1

--- aws-app-cdk/lib/aws-app-cdk-stack.ts
+++ aws-app-cdk/lib/aws-app-cdk-stack.ts
@@ -4,7 +4,9 @@ import * as ecs_patterns from 'aws-cdk-lib/aws-ecs-patterns';
 import { Construct } from 'constructs';
 import {EcrImage} from "aws-cdk-lib/aws-ecs";
 import {Repository} from "aws-cdk-lib/aws-ecr";
-import {CfnOutput, Duration, Stack, StackProps} from "aws-cdk-lib";
+import {aws_s3, aws_s3_deployment, CfnOutput, Duration, Stack, StackProps} from "aws-cdk-lib";
+import {BlockPublicAccess} from "aws-cdk-lib/aws-s3";
+import * as path from "path";
 
 export interface ServiceStackProps extends StackProps {
     imageTag: string
@@ -15,44 +17,44 @@ export class AwsAppCdkStack extends Stack {
     super(scope, id, props);
 
 
-   const vpc = new ec2.Vpc(this, "MyVpc",{
-       maxAzs: 3
-   });
+    const bucket = new aws_s3.Bucket(this, "s3BucketTest",{
+        bucketName: "helango-udemy-cdk-test",
+        blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+        }
+    )
+      const deployBucket = new aws_s3_deployment.BucketDeployment(this, "deployBucket",{
+          sources: [aws_s3_deployment.Source.asset(path.join(__dirname, '../s3uploads'))],
+          destinationBucket: bucket
+          }
 
-   const cluster = new ecs.Cluster(this, "MyCluster",{
-       vpc: vpc
-   });
-
-      const ecrRepo = Repository.fromRepositoryArn(
-          this,
-          'ecr-repo',
-          `arn:aws:ecr:us-west-2:294541522848:repository/reactive-music`)
-
-      const fargateAlbService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService",{
-       cluster: cluster,
-       cpu: 256,
-       healthCheckGracePeriod: Duration.seconds(120),
-       desiredCount: 2,
-       memoryLimitMiB: 512,
-       publicLoadBalancer: true,
-       taskImageOptions: { image: EcrImage.fromEcrRepository(ecrRepo,props?.imageTag)},
-   })
-
-     // const scalableTarget = fargateAlbService.service.autoScaleTaskCount({
-     //      minCapacity:1,
-     //      maxCapacity:4
-     //  })
-     //  scalableTarget.scaleOnCpuUtilization("",{
-     //      targetUtilizationPercent: 50
-     //  })
-     //  scalableTarget.scaleOnMemoryUtilization("",{
-     //      targetUtilizationPercent:50
-     //  })
-
-   fargateAlbService.targetGroup.configureHealthCheck({path:'/actuator/health', timeout: Duration.seconds(30), interval: Duration.seconds(60)})
-      new CfnOutput(this, "loadBalancer" , {
-          exportName: "lburl",
-          value: fargateAlbService.loadBalancer.loadBalancerDnsName
-      })
-  }
+      )
+  //  const vpc = new ec2.Vpc(this, "MyVpc",{
+  //     maxAzs: 3
+  //  });
+  //
+  //  const cluster = new ecs.Cluster(this, "MyCluster",{
+  //       vpc: vpc
+  //  });
+  //
+  //     const ecrRepo = Repository.fromRepositoryArn(
+  //         this,
+  //         'ecr-repo',
+  //         `arn:aws:ecr:us-west-2:294541522848:repository/reactive-music`)
+  //
+  //     const fargateAlbService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService",{
+  //      cluster: cluster,
+  //      cpu: 256,
+  //      healthCheckGracePeriod: Duration.seconds(120),
+  //      desiredCount: 2,
+  //      memoryLimitMiB: 512,
+  //      publicLoadBalancer: true,
+  //      taskImageOptions: { image: EcrImage.fromEcrRepository(ecrRepo,props?.imageTag)},
+  //  })
+  //
+  //  fargateAlbService.targetGroup.configureHealthCheck({path:'/actuator/health', timeout: Duration.seconds(30), interval: Duration.seconds(60)})
+  //     new CfnOutput(this, "loadBalancer" , {
+  //         exportName: "lburl",
+  //         value: fargateAlbService.loadBalancer.loadBalancerDnsName
+  //     })
+   }
 }
